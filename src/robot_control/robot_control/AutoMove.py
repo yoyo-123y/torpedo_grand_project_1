5fae89e8e0e7211c2c512000b2ad5faefb2c06ce
Youssif roushdy <youssifalsayad5656@gmail.com>
2025-10-25 04:14:35 +0300

    added pid

--- src/robot_control/robot_control/AutoMove.py
+++ src/robot_control/robot_control/AutoMove.py
@@ -9,8 +9,24 @@ class AutoMove(Node):
         self.subscription = self.create_subscription(Int32MultiArray, 'line_sensors', self.follow_line, 10)
         self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
         self.wait_for_topic('line_sensors', timeout=10.0)
-
+        self.kp = 1.0
+        self.ki = 0.0
+        self.kd = 0.2
+        self.prev_error = 0.0
+        self.integral = 0.0
+        self.prev_time = time.time()
         self.connected = False
+    def compute_pid(self, error):
+        current_time = time.time()
+        dt = current_time - self.prev_time
+        self.prev_time = current_time
+
+        self.integral += error * dt
+        self.integral = max(min(self.integral, 10.0), -10.0)
+        derivative = (error - self.prev_error) / dt if dt > 0 else 0.0
+        self.prev_error = error
+
+        return self.kp * error + self.ki * self.integral + self.kd * derivative
     def wait_for_topic(self, topic_name, timeout=10.0):
         start_time = time.time()
 
@@ -24,20 +40,16 @@ class AutoMove(Node):
 
     def follow_line(self,msg):
         left, right = msg.data
+        error = right - left
+        correction = self.compute_pid(error)
         twist = Twist()
 
         if left == 0 and right == 0:
             twist.linear.x = 0.5
             twist.angular.z = 0.0
-        elif left == 0 and right == 1:
-            twist.linear.x = 0.2
-            twist.angular.z = 0.5
-        elif left == 1 and right == 0:
-            twist.linear.x = 0.2
-            twist.angular.z = -0.5
         else:
-            twist.linear.x = 0.0
-            twist.angular.z = 0.0
+            twist.linear.x = 0.3
+            twist.angular.z = correction
 
         self.publisher.publish(twist)
         self.get_logger().info(f"Line state L={left} R={right}")
